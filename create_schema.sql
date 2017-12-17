7a7dcad809b13bd26f0e3fba59acdf43a5cc5aa1
Omer Barak <omeramosbarak@gmail.com>
2017-12-17 15:29:30 +0200

    Actually ran the schema through mysql and fixed things

--- create_schema.sql
+++ create_schema.sql
@@ -1,11 +1,12 @@
 CREATE SCHEMA `top_music`;
+USE top_music;
 
 CREATE TABLE Songs (
 	song_id INT,
 	artist_id INT,
 	name VARCHAR(100),
-	genre GENRE,
-	release Date,
+	genre INT,
+	release_date DATE,
 	PRIMARY KEY (song_id)
 );
 
@@ -17,28 +18,28 @@ CREATE TABLE Artist (
 );
 
 CREATE TABLE Chart (
-	date Date,
+	chart_date DATE,
 	song_id INT,
 	artist_id INT,
 	position INT,
-	FOREIGN KEY (song_id) REFERENCES (Songs.song_id)
-	FOREIGN KEY (artist_id) REFERENCES (Artist.artist_id)
+	FOREIGN KEY (song_id) REFERENCES Songs(song_id),
+	FOREIGN KEY (artist_id) REFERENCES Artist(artist_id)
 );
-CREATE INDEX chart_date_idx ON Chart(date);
+CREATE INDEX chart_date_idx ON Chart(chart_date);
 CREATE INDEX chart_song_idx ON Chart(song_id);
 CREATE INDEX chart_artist_idx ON Chart(artist_id);
 
 CREATE TABLE RelatedArtists (
 	single INT,
 	band INT,
-	FOREIGN KEY (single) REFERENCES (Artist.artist_id)
-	FOREIGN KEY (band) REFERENCES (Artist.artist_id)
+	FOREIGN KEY (single) REFERENCES Artist(artist_id),
+	FOREIGN KEY (band) REFERENCES Artist(artist_id)
 );
-CREATE INDEX related_single_idx ON RelatedArtists(single_aritst);
+CREATE INDEX related_single_idx ON RelatedArtists(single);
 CREATE INDEX related_band_idx ON RelatedArtists(band);
 
 CREATE TABLE Lyrics (
-	song_id INT
-	lyrics VARCHAR(MAX)
-	FOREIGN KEY (song_id) REFERENCES (Songs.song_id)
+	song_id INT,
+	lyrics VARCHAR(21840),
+	FOREIGN KEY (song_id) REFERENCES Songs(song_id)
 );
