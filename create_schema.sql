211c01230ca0de7baf200fabceb54f63b17b3673
Omer Barak <omeramosbarak@gmail.com>
2017-12-26 16:03:54 +0200

    Some more parsing

--- create_schema.sql
+++ create_schema.sql
@@ -13,8 +13,10 @@ CREATE TABLE Songs (
 CREATE TABLE Artist (
 	artist_id INT,
 	artist_name VARCHAR(50),
-	is_single BIT(1),
-	PRIMARY KEY (artist_id)
+	source_contry INT,
+	is_solo BIT(1),
+	PRIMARY KEY (artist_id),
+	FOREIGN KEY (artist_id) REFERENCES Artist(artist_id)
 );
 
 CREATE TABLE Chart (
@@ -30,16 +32,22 @@ CREATE INDEX chart_song_idx ON Chart(song_id);
 CREATE INDEX chart_artist_idx ON Chart(artist_id);
 
 CREATE TABLE RelatedArtists (
-	single INT,
+	solo INT,
 	band INT,
-	FOREIGN KEY (single) REFERENCES Artist(artist_id),
+	FOREIGN KEY (solo) REFERENCES Artist(artist_id),
 	FOREIGN KEY (band) REFERENCES Artist(artist_id)
 );
-CREATE INDEX related_single_idx ON RelatedArtists(single);
+CREATE INDEX related_solo_idx ON RelatedArtists(solo);
 CREATE INDEX related_band_idx ON RelatedArtists(band);
 
 CREATE TABLE Lyrics (
 	song_id INT,
 	lyrics VARCHAR(21840),
 	FOREIGN KEY (song_id) REFERENCES Songs(song_id)
+);
+
+CREATE TABLE Contries (
+	country_id INT,
+	country_name VARCHAR(64),
+	PRIMARY KEY (country_id)
 );
