3a0c7fc64ab4982dbfbb00d20a8658ab7a31b4f2
matan <matanfillus@gmail.com>
2018-01-06 16:58:41 +0200

    Added methods for extracting data from billboard.com. Also fixed some bugs in general methods. Note that right now we base the artist name of a song on the first named artist in the credits of billboard.com (for example, "Perfect" is credited as a Duet between Ed Sheeran and Beyonce, but we only credit Ed Sheeran). This is done since almost all the artists names are some mish mash of "Featuring/Duet/X/+" etc., strings that will not correlate with input extracted from the other APIs. Also note that for although the pull_week method is fully functional, it is easier to extract data using the extract_billboard_charts method, which simply extracts the last number of charts per week backwards.

--- create_schema.sql
+++ create_schema.sql
@@ -51,3 +51,55 @@ CREATE TABLE Countries (
 	country_name VARCHAR(64),
 	PRIMARY KEY (country_id)
 );
+=======
+USE top_music;
+
+CREATE TABLE Songs (
+	song_id INT NOT NULL AUTO_INCREMENT,
+	artist_id INT,
+	name VARCHAR(100) UNIQUE,
+	release_date DATE,
+	PRIMARY KEY (song_id)
+);
+
+CREATE TABLE Artist (
+	artist_id INT NOT NULL AUTO_INCREMENT,
+	artist_name VARCHAR(50) UNIQUE,
+	source_contry INT,
+	is_solo BIT(1),
+	mb_id VARCHAR(36),
+	PRIMARY KEY (artist_id)
+);
+
+CREATE TABLE Chart (
+	chart_date DATE,
+	song_id INT,
+	artist_id INT,
+	position INT,
+	FOREIGN KEY (song_id) REFERENCES Songs(song_id),
+	FOREIGN KEY (artist_id) REFERENCES Artist(artist_id)
+);
+CREATE INDEX chart_date_idx ON Chart(chart_date);
+CREATE INDEX chart_song_idx ON Chart(song_id);
+CREATE INDEX chart_artist_idx ON Chart(artist_id);
+
+CREATE TABLE RelatedArtists (
+	solo INT NOT NULL,
+	band INT NOT NULL,
+	FOREIGN KEY (solo) REFERENCES Artist(artist_id),
+	FOREIGN KEY (band) REFERENCES Artist(artist_id)
+);
+CREATE INDEX related_solo_idx ON RelatedArtists(solo);
+CREATE INDEX related_band_idx ON RelatedArtists(band);
+
+CREATE TABLE Lyrics (
+	song_id INT,
+	lyrics VARCHAR(21840),
+	FOREIGN KEY (song_id) REFERENCES Songs(song_id)
+);
+
+CREATE TABLE Countries (
+	country_id INT NOT NULL AUTO_INCREMENT,
+	country_name VARCHAR(64),
+	PRIMARY KEY (country_id)
+);
