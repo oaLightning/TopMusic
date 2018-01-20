37c1520076bd80db08f4732024aa3e42aaa4045d
Omer Barak <omeramosbarak@gmail.com>
2018-01-20 12:33:09 +0200

    Fixed the lyrics changing

--- fix_lyrics.py
+++ fix_lyrics.py
@@ -42,7 +42,7 @@ def fix_db_lyrics():
     with db_cursor(True) as cursor:
         for row in rows:
             original = row[1]
-            fixed_row = re.sub('^Paroles de la chanson .+ par .+$', '', row[1], re.MULTILINE)
+            fixed_row = re.sub('^Paroles de la chanson .+ par .+\r\n', '', original, re.MULTILINE)
             if fixed_row != original:
                 print 'Changing lyrics for song id ' + str(row[0])
                 full_line = update % (row[0], fixed_row)
